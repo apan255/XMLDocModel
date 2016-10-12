30fd52f79cb118134fba7c5f2883c70b6c65833e
Aditya Pandey <apandey6@ncsu.edu>
2016-10-12 12:14:05 -0700

    Update XmlDocument.cpp

--- XMLDocument/XMLDocument/XmlDocument.cpp
+++ XMLDocument/XMLDocument/XmlDocument.cpp
@@ -1,10 +1,7 @@
 ///////////////////////////////////////////////////////////////////
 // XmlDocument.cpp - a container of XmlElement nodes             //
 // Ver 1.4                                                       //
-// Application: Help for CSE687 Pr#2, Spring 2015                //
-// Platform:    Dell XPS 2720, Win 8.1 Pro, Visual Studio 2013   //
-// Author:      Jim Fawcett, CST 4-187, 443-3948                 //
-//              jfawcett@twcny.rr.com                            //
+
 ///////////////////////////////////////////////////////////////////
 
 #include <iostream>
